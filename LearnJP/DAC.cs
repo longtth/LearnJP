3ae8b22325ca745a32ff2339150f878a2b95c9fe
longtth <longtth@users.noreply.github.com>
2018-05-03 23:20:48 +0700

    Import solution to Github

---
+++ LearnJP/DAC.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnJP
+{
+    public class DAC
+    {
+        public List<MyWord> ReadFromFile(string path)
+        {
+            return null; 
+        }
+
+        public int SaveToFile(string path, List<MyWord> list)
+        {
+            return 0; 
+        }
+    }
+}
---
+++ LearnJP/MyWord.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnJP
+{
+    public class MyWord
+    {
+        public MyWord()
+        {
+            Kanji = "";
+            Furigana = "";
+            HanViet = "";
+            Meaning = "";
+            Sample = "";
+        }
+
+        public MyWord(string kanji, string furigana, string hanViet, string meaning, string sample)
+        {
+            Kanji = kanji;
+            Furigana = furigana;
+            HanViet = hanViet;
+            Meaning = meaning;
+            Sample = sample;
+        }
+
+        public string Kanji { get; set;  }
+        public string Furigana { get; set; }
+        public string HanViet { get; set; }
+        public string Meaning { get; set; }
+        public string Sample { get; set; }
+
+    }
+}
